53ef622759c7fdea8316fece1b3257d7f73d952e
Kolen Cheung <christian.kolen@gmail.com>
2017-01-07 12:00:12 -0800

    initial commit

---
+++ 2zip.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# install 7z
+sudo apt-get install p7zip-full
+
+# names
+DEB=pandoc-1.19.1-1-amd64.deb
+PKG=pandoc-1.19.1-osx.pkg
+MSI=pandoc-1.19.1-windows.msi
+
+# Linux
+ar x $DEB data.tar.gz && mv data.tar.gz ${DEB%.*}.tar.gz
+
+# osx
+7z x $PKG -oosx
+cat osx/pandoc.pkg/Payload | gunzip -dc |cpio -i
+zip -r ${PKG%.*}.zip osx/pandoc.pkg/usr/
+
+# Windows
+7z x $MSI -owindows
+zip -r ${MSI%.*}.zip windows
+# 7z x $MSI -owindows && cd windows && 7z a ../${MSI%.*}.zip && cd ..
