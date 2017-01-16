e3285c9e37774df462d10c207e37aa91adf4d350
Kolen Cheung <christian.kolen@gmail.com>
2017-01-16 03:19:37 -0800

    2zip.sh: cleanup

--- 2zip.sh
+++ 2zip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# usage: VERSION=1.19.1 ./2zip.sh
+# usage: VERSION=1.19.1 source ./2zip.sh # source for running it in the current shell such that the variables can be obtained
 
 # Notes on installing 7z
 # Linux: `sudo apt-get install p7zip-full`
@@ -17,34 +17,50 @@ msiUrlPartial=$(curl -L $url | grep -o '/jgm/pandoc/releases/download/.*\.msi')
 # download and extract
 ## Linux
 if [[ ! -z "$debUrlPartial" ]]; then
+  # variables
   debUrl="https://github.com$debUrlPartial"
   DEB=${debUrl##*/}
+  debWoExt=${DEB%.*}
+  debZip=$debWoExt.zip
+  # download
   wget $debUrl
   # unpack
-  ar x $DEB data.tar.gz && mv data.tar.gz ${DEB%.*}.tar.gz
+  ar x $DEB data.tar.gz && mv data.tar.gz $debWoExt.tar.gz
+  tar -zxvf $debWoExt.tar.gz && mv usr $debWoExt
+  # zip
+  zip -r $debZip $debWoExt
 fi
 ## macOS
 if [[ ! -z "$pkgUrlPartial" ]]; then
+  # variables
   pkgUrl="https://github.com$pkgUrlPartial"
   PKG=${pkgUrl##*/}
+  pkgWoExt=${PKG%.*}
+  pkgZip=$pkgWoExt.zip
+  # download
   wget $pkgUrl
   # unpack
-  7z x $PKG -oosx
-  cat osx/pandoc.pkg/Payload | gunzip -dc |cpio -i
-  zip -r ${PKG%.*}.zip usr/
+  7z x $PKG -o"$pkgWoExt"
+  cat $pkgWoExt/pandoc.pkg/Payload | gunzip -dc |cpio -i && mv usr $pkgWoExt
+  # zip
+  zip -r $pkgZip $pkgWoExt
 fi
 ## Windows
 if [[ ! -z "$msiUrlPartial" ]]; then
+  # variables
   msiUrl="https://github.com$msiUrlPartial"
   MSI=${msiUrl##*/}
+  msiWoExt=${MSI%.*}
+  msiZip=$msiWoExt.zip
+  # download
   wget $msiUrl
   # unpack
-  7z x $MSI -o${MSI%.*}
-  zip -r ${MSI%.*}.zip ${MSI%.*}
+  7z x $MSI -o"$msiWoExt"
+  # zip
+  zip -r $msiZip $msiWoExt
 fi
 
 # debug
 # echo $msiUrl
 # echo $pkgUrl
 # echo $msiUrl
-
