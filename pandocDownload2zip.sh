c457d3fecf16e28cde882df2494a3497dc422a14
Kolen Cheung <christian.kolen@gmail.com>
2017-01-21 22:05:00 -0800

    pandocDownload2zip.sh: quoting all strings
    in case there are spaces

--- pandocDownload2zip.sh
+++ pandocDownload2zip.sh
@@ -8,30 +8,30 @@
 ## macOS: `brew install msitools`
 
 # debug msg
-if [[ $DEBUG == "true" ]]; then
-  echo $VERSION
+if [[ "$DEBUG" == "true" ]]; then
+  echo "$VERSION"
 fi
 
 # url to pandoc version
 url="https://github.com/jgm/pandoc/releases/tag/$VERSION"
 # debug msg
-if [[ $DEBUG == "true" ]]; then
-  echo $url
+if [[ "$DEBUG" == "true" ]]; then
+  echo "$url"
 fi
 
 # get url to pandoc binaries
-debUrlPartial=$(curl -L $url | grep -o '/jgm/pandoc/releases/download/.*\.deb')
-pkgUrlPartial=$(curl -L $url | grep -o '/jgm/pandoc/releases/download/.*\.pkg')
-msiUrlPartial=$(curl -L $url | grep -o '/jgm/pandoc/releases/download/.*\.msi')
-zipUrlPartial=$(curl -L $url | grep -o '/jgm/pandoc/archive/.*\.zip')
-tarUrlPartial=$(curl -L $url | grep -o '/jgm/pandoc/archive/.*\.tar\.gz')
+debUrlPartial=$(curl -L "$url" | grep -o '/jgm/pandoc/releases/download/.*\.deb')
+pkgUrlPartial=$(curl -L "$url" | grep -o '/jgm/pandoc/releases/download/.*\.pkg')
+msiUrlPartial=$(curl -L "$url" | grep -o '/jgm/pandoc/releases/download/.*\.msi')
+zipUrlPartial=$(curl -L "$url" | grep -o '/jgm/pandoc/archive/.*\.zip')
+tarUrlPartial=$(curl -L "$url" | grep -o '/jgm/pandoc/archive/.*\.tar\.gz')
 # debug msg
-if [[ $DEBUG == "true" ]]; then
-  echo $debUrlPartial
-  echo $pkgUrlPartial
-  echo $msiUrlPartial
-  echo $zipUrlPartial
-  echo $tarUrlPartial
+if [[ "$DEBUG" == "true" ]]; then
+  echo "$debUrlPartial"
+  echo "$pkgUrlPartial"
+  echo "$msiUrlPartial"
+  echo "$zipUrlPartial"
+  echo "$tarUrlPartial"
 fi
 
 # prepare dist folder
@@ -42,78 +42,78 @@ mkdir -p dist
 if [[ ! -z "$debUrlPartial" ]]; then
   # variables
   debUrl="https://github.com$debUrlPartial"
-  DEB=${debUrl##*/}
-  debWoExt=${DEB%.*}
-  debZip=$debWoExt.zip
+  DEB="${debUrl##*/}"
+  debWoExt="${DEB%.*}"
+  debZip="$debWoExt.zip"
   # download
-  wget $debUrl
+  wget "$debUrl"
   # unpack
-  ar x $DEB data.tar.gz && mv data.tar.gz $debWoExt.tar.gz
-  tar -zxvf $debWoExt.tar.gz && mv usr $debWoExt
+  ar x "$DEB" data.tar.gz && mv data.tar.gz "$debWoExt.tar.gz"
+  tar -zxvf "$debWoExt.tar.gz" && mv usr "$debWoExt"
   # zip
-  zip -r $debZip $debWoExt
+  zip -r "$debZip" "$debWoExt"
   # to dist/
-  mv $DEB dist/
-  mv $debZip dist/
-  mv $debWoExt.tar.gz dist/
+  mv "$DEB" dist/
+  mv "$debZip" dist/
+  mv "$debWoExt.tar.gz" dist/
 fi
 ## macOS
 if [[ ! -z "$pkgUrlPartial" ]]; then
   # variables
   pkgUrl="https://github.com$pkgUrlPartial"
-  PKG=${pkgUrl##*/}
-  pkgWoExt=${PKG%.*}
-  pkgZip=$pkgWoExt.zip
+  PKG="${pkgUrl##*/}"
+  pkgWoExt="${PKG%.*}"
+  pkgZip="$pkgWoExt.zip"
   # download
-  wget $pkgUrl
+  wget "$pkgUrl"
   # unpack
   if [[ `uname` == "Darwin" ]]; then
-    mkdir $pkgWoExt-pkg && xar -xf $PKG -C $pkgWoExt-pkg
+    mkdir "$pkgWoExt-pkg" && xar -xf "$PKG" -C "$pkgWoExt-pkg"
   else
-    7z x $PKG -o"$pkgWoExt-pkg"
+    7z x "$PKG" -o"$pkgWoExt-pkg"
   fi
-  cat $pkgWoExt-pkg/pandoc.pkg/Payload | gunzip -dc |cpio -i && mv usr $pkgWoExt
+  cat "$pkgWoExt-pkg/pandoc.pkg/Payload" | gunzip -dc |cpio -i && mv usr "$pkgWoExt"
   # zip
-  zip -r $pkgZip $pkgWoExt
+  zip -r "$pkgZip" "$pkgWoExt"
   # to dist/
-  mv $PKG dist/
-  mv $pkgZip dist/
+  mv "$PKG" dist/
+  mv "$pkgZip" dist/
 fi
 ## Windows
 if [[ ! -z "$msiUrlPartial" ]]; then
   # variables
   msiUrl="https://github.com$msiUrlPartial"
-  MSI=${msiUrl##*/}
-  msiWoExt=${MSI%.*}
-  msiZip=$msiWoExt.zip
+  MSI="${msiUrl##*/}"
+  msiWoExt="${MSI%.*}"
+  msiZip="$msiWoExt.zip"
   # download
-  wget $msiUrl
+  wget "$msiUrl"
   # unpack
-  msiextract $MSI && mv "Program Files/Pandoc" $msiWoExt && rm -rf "Program Files"
+  msiextract "$MSI" && mv "Program Files/Pandoc" "$msiWoExt" && rm -rf "Program Files"
   # zip
-  zip -r $msiZip $msiWoExt
+  zip -r "$msiZip" "$msiWoExt"
   # to dist/
-  mv $MSI dist/
-  mv $msiZip dist/
+  mv "$MSI" dist/
+  mv "$msiZip" dist/
 fi
 ## Source Code
 ### .zip
 if [[ ! -z "$zipUrlPartial" ]]; then
   # variables
   zipUrl="https://github.com$zipUrlPartial"
-  ZIP=${zipUrl##*/}
+  ZIP="${zipUrl##*/}"
   # download
-  wget $zipUrl
+  wget "$zipUrl"
 	# rename & to dist/
-	mv $ZIP dist/pandoc-source-code-$ZIP
+	mv "$ZIP" "dist/pandoc-source-code-$ZIP"
 fi
 ### .tar.gz
 if [[ ! -z "$tarUrlPartial" ]]; then
   # variables
   tarUrl="https://github.com$tarUrlPartial"
-  TAR=${tarUrl##*/}
+  TAR="${tarUrl##*/}"
   # download
-  wget $tarUrl
+  wget "$tarUrl"
 	# rename & to dist/
-  mv $TAR dist/pandoc-source-code-$TAR
+  mv "$TAR" "dist/pandoc-source-code-$TAR"
 fi
